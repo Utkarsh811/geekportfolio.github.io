298f217c6f874509b83871f24ef17662bdb8033b
Falcon <76834132+Utkarsh811@users.noreply.github.com>
2021-09-09 22:48:51 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,12 +1,12 @@
 document.getElementsByClassName('hamburger')[0].addEventListener('click', (e) => {
       const nav = document.getElementById('mobile-nav');
       const icon = document.getElementsByClassName('hamburger')[0];
-      const img = document.getElementById('img-mobile');
+      #const img = document.getElementById('img-mobile');
       const root = document.getElementById('root');
       if(nav.style.display !== 'flex'){
         nav.style.display = 'flex';
         icon.classList.remove('fa-bars');
-        icon.classList.add('fa-times');
+        #icon.classList.add('fa-times');
         img.style.display = 'none';
         root.style.backgroundImage = 'none';
       }else{
